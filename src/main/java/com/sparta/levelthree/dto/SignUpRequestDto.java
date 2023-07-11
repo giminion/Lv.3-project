3cb2484c60ce0bbc2a465b3f7c21433a9171ef2b
안민현 <minhyun17@naver.com>
2023-07-11 17:59:06 +0900

    feat. password 특수문자 포함

--- src/main/java/com/sparta/levelthree/dto/SignUpRequestDto.java
+++ src/main/java/com/sparta/levelthree/dto/SignUpRequestDto.java
@@ -9,6 +9,6 @@ import lombok.Setter;
 public class SignUpRequestDto {
     @Pattern(regexp = "[a-z0-9]{4,10}")
     private String username;
-    @Pattern(regexp = "[a-zA-Z0-9]{8,15}")
+    @Pattern(regexp = "[a-zA-Z0-9'~!@#$%^&*()-_=+]{8,15}")  // 특수문자 포함되도록 변경
     private String password;
 }
