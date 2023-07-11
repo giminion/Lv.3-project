bcc32de079873e85604217ba9ed0c2cf45b2ab08
안민현 <minhyun17@naver.com>
2023-07-11 16:59:37 +0900

    initial commit

---
+++ src/main/java/com/sparta/levelthree/LevelThreeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.levelthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevelThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevelThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/levelthree/LevelThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.levelthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
